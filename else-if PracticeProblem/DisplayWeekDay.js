fa15929ae76aeed16fa2841aef947a031717a16b
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-04 01:35:08 +0530

    [mihir] Added: printing day of a week.

---
+++ else-if PracticeProblem/DisplayWeekDay.js
@@ -0,0 +1,41 @@
+"use strict"
+
+//For promting message and taking input from console.
+const prompt = require("prompt-sync")({ sigint: true });
+
+let userNumber = prompt("Enter number between 1 to 7 : ");
+
+let weekNumber = + userNumber;
+
+if(weekNumber == 1)
+{
+    console.log("ToDay is Sunday");
+}
+else if(weekNumber == 2)
+{
+    console.log("ToDay is Monday");
+}
+else if(weekNumber == 2)
+{
+    console.log("ToDay is Tuesday");
+}
+else if(weekNumber == 2)
+{
+    console.log("ToDay is Wednesday");
+}
+else if(weekNumber == 2)
+{
+    console.log("ToDay is Thursday");
+}
+else if(weekNumber == 2)
+{
+    console.log("ToDay is Friday");
+}
+else if(weekNumber == 7)
+{
+    console.log("ToDay is Saturday");
+}
+else
+{
+    console.log("Wrong Input");
+}
