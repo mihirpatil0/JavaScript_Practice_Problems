8700f63471827ebf0c47d8de96976f632b3ced58
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-04 10:10:32 +0530

    [mihir] Added: printing power of two table.

---
+++ For-Loop PracticeProblem/PowerOfTwo.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//For promting message and taking input from console.
+const prompt = require("prompt-sync")({ sigint: true });
+
+let base = prompt("Enter the base number : ");
+let exponent = prompt("Enter the exponent number : ");
+
+let baseNumber = + base;
+let exponentNumber = + exponent;
+
+
+for(let i = 0; i <= exponentNumber; i++)
+{
+    console.log(2 + " to the power of " + i + " is " + Math.pow(baseNumber, i));
+}
